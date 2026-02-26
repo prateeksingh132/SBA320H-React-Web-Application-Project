ab1609806cfc748984c9e939b363e2559165035f
prateeksingh132 <kumarpks132@gmail.com>
2026-02-25 22:08:53 -0700

    created basic homepage and added react-router-dom in app.jsx. wrapped main in browserrouter. tested it on browser, routing works well so far... going to work on the navbar next..

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+// testing: setting up a basic homepage to make sure my routing works before i add other stuff
+
+const HomePage = () => {
+
+    ////////////TESTING
+    // console.log('TESTING: homepage component is rendering');
+    ////////////
+
+    return (
+        <div className="main_container">
+            <h1>welcome to gadgetshack</h1>
+            <p>fdsfsdsdsdsdsdsdsdsdsd</p>
+        </div>
+    );
+};
+
+export default HomePage;
